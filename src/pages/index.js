17708bd3fb5bca60fd21d560065a42098dc0a891
Jon <joninvermont@gmail.com>
2022-07-20 01:20:22 -0400

    revert test text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ function Home() {
       </Head>
       <main className="h-screen flex flex-col 2xl:flex-row items-center justify-between">
         <div className="h-1/3 2xl:h-full w-full 2xl:w-1/3 bg-black text-white flex items-center justify-center">
-          <span>Deck 1 test change to dev branch</span>
+          <span>Deck 1</span>
         </div>
         <div className="h-1/3 2xl:h-full w-full 2xl:w-1/3 flex items-center justify-center">
           <span>Control Panel</span>
